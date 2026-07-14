8781e1a5bed13c5dacf42bb1136dff3d648d2dad
Melipax <pmfura@alumni.cmu.edu>
2026-07-14 12:52:46 -0600

    Revert "Implement config-driven EV exclusion + standardize POWERTRAIN_TYPE category"
    This reverts commit 5ebe2b2ac804beccda7a2ff0e2fe2da169055cab.

--- accy_v2/model_lookup/search_engine.py
+++ accy_v2/model_lookup/search_engine.py
@@ -133,7 +133,6 @@ class VehicleSearchEngine:
             csv_path=self.csv_path,
             exclude_ev=exclude_ev,
             configs_dir=self.configs_dir,
-            oem_config=self.oem_config,
         )
 
         candidate_count = len(results)
